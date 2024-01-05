436d3080c2a586eb69e2409ad143481b512b8581
OmerMor1 <omer6688@gmail.com>
2024-01-05 13:52:51 +0200

    test path 5

--- client/src/App.js
+++ client/src/App.js
@@ -11,11 +11,11 @@ import emailjs from 'emailjs-com';
 
 function App() {
   emailjs.init('37xiDGS3H-A-ucKOu');
-
+const name="/first-web";
 
     return (
       <div className="App">
-        <Router basename="/first-web">
+        <Router basename={name.PUBLIC_URL}>
           <Header />
           <Routes >
             <Route path="/" element={<Home />} />
